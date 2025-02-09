4faf331eaea3ede5a45afbcf8b02af309020b67f
forplacement90 <Sourav6239@@>
2025-02-09 17:59:36 +0530

    init create

---
+++ init/data.js
@@ -0,0 +1,10 @@
+const sampleBlogs = [
+    {
+        username: "sujit@28",
+        title: "Goa Beach",
+        description:
+          "Enjoy stunning ocean views and easy access to the beach.",
+        image:
+          "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    }
+];
