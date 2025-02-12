d8c9e06c941c53678474c6f6dbcdcacae7d98899
sourav kumar <souravkumar85055@gmail.com>
2025-02-12 11:52:22 +0530

    new added

--- init/data.js
+++ init/data.js
@@ -5,62 +5,62 @@ const sampleBlogs = [
         description:
           "Enjoy stunning ocean views and easy access to the beach.",
         image:
-          "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+          "https://thumbs.dreamstime.com/b/sea-sunset-landscape-imaage-61996273.jpg",
     },
 
       {
           username: "rahul@12",
           title: "Maldives Escape",
           description: "Experience luxury overwater bungalows with crystal-clear waters.",
-          image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://images5.alphacoders.com/771/thumb-1920-771834.jpg"
       },
       {
           username: "anita@34",
           title: "Swiss Alps",
           description: "Snow-capped mountains and breathtaking landscapes await you.",
-          image: "https://images.unsplash.com/photo-1524518411170-6f43a41c2453?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://tse3.mm.bing.net/th?id=OIP.AKsGWau2J0MvQ5wSH98B7AHaE7&pid=Api&P=0&h=220"
       },
       {
           username: "dev@56",
           title: "Bali Retreat",
           description: "Peaceful resorts surrounded by lush green forests and waterfalls.",
-          image: "https://images.unsplash.com/photo-1580250061801-18f4f1d1a6c5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://tse1.mm.bing.net/th?id=OIP.LCp7BCwYnA-Bd4Cmuj4TLAHaEe&pid=Api&P=0&h=220"
       },
       {
           username: "sam@77",
           title: "Paris Getaway",
           description: "Romantic evenings with views of the Eiffel Tower.",
-          image: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://img.freepik.com/premium-photo/little-boy-with-airplaneboy-airplane_1016228-1034.jpg"
       },
       {
           username: "sneha@88",
           title: "Santorini Views",
           description: "Whitewashed houses and blue-domed churches by the sea.",
-          image: "https://images.unsplash.com/photo-1504867960501-0146086b8a48?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://yourjourneytopeace.com/wp-content/uploads/2017/05/Imaage-article-ocean-846084__480.jpg"
       },
       {
           username: "vikas@99",
           title: "Tokyo Nights",
           description: "Explore vibrant city lights and rich Japanese culture.",
-          image: "https://images.unsplash.com/photo-1576485436509-6e6d00893784?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://tse4.mm.bing.net/th?id=OIP.KsygcuWfZBRPGx_IH7PKWgHaEK&pid=Api&P=0&h=220"
       },
       {
           username: "maya@23",
           title: "Hawaii Adventure",
           description: "Tropical paradise with volcanoes, beaches, and waterfalls.",
-          image: "https://images.unsplash.com/photo-1536111103082-491d59c163f2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://tse3.mm.bing.net/th?id=OIP.PQQsktmxZ8Bl1Idclk0LSgHaEo&pid=Api&P=0&h=220"
       },
       {
           username: "rohit@11",
           title: "New York Skyline",
           description: "Stunning cityscapes and the bustling streets of NYC.",
-          image: "https://images.unsplash.com/photo-1512034400317-de97d7d6c3ed?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://tse4.mm.bing.net/th?id=OIP.ybKRj9NJKi49Fx2wiDaLnwHaEo&pid=Api&P=0&h=220"
       },
       {
           username: "nina@45",
           title : "Dubai Luxury",
           description: "Skyscrapers, luxury shopping, and desert safaris.",
-          image: "https://images.unsplash.com/photo-1529113240125-0477e7f0b67a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800"
+          image: "https://tse3.mm.bing.net/th?id=OIP.nYQTtBOrm6ufPsEL4UPOcAHaE7&pid=Api&P=0&h=220"
       },
      
 ];
